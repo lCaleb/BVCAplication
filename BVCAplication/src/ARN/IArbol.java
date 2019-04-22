be6394f97b5c9017a326e566f7ce8fa9d310b3fc
caleb <lcaleblhenao@gmail.com>
2019-04-21 21:35:43 -0500

    interfaz del arbol

---
+++ BVCAplication/src/ARN/IArbol.java
@@ -0,0 +1,28 @@
+package ARN;
+
+public interface IArbol<T>
+{
+
+/**
+* Retorna la altura del árbol.
+* @return La altura del árbol.
+*/
+public int darAltura();
+
+/**
+* Retorna el número de elementos del árbol.
+* @return El número de elementos del árbol.
+*/
+public int darPeso();
+
+/**
+* Busca un elemento en el árbol dado su modelo.
+* @param modelo Descripción del elemento que se va a buscar en el árbol. Debe contener por lo menos la información mínima necesaria para que el método de comparación del
+*        nodo pueda establecer una relación de orden.
+* @return T elemento del árbol que corresponde al modelo o <code>null</code> si este no existe.
+*/
+public T buscar(T modelo);
+
+}
+
+
